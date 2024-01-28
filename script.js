c4b6cde3c84769e754c35c0e7bf38ed38023700f
arios8 <154016447+arios8@users.noreply.github.com>
2024-01-28 17:52:35 +0000

    shapes

--- script.js
+++ script.js
@@ -3,14 +3,20 @@ function setup(){
 }
 
 function draw(){
-  background(176, 228, 247);
+  //background(2, 3, 38);
+  background(250, 247, 249);
   // add your drawing code here ...
   stroke(50,50);
+  triangle(250,200,250);
+  line(50,200);
+  ellipse(300,200,200);
+  fill(255, 179, 237);
+
+
   //width,height, circle size
   //width/2 , height/2 = middle of canvas
-  ellipse(150,150,50);
   //three points of shape
-  triangle(width/2 - 45, height/2 - 55, width/2 - 50, height/2 - 45);
+  //triangle(width/2 - 45, height/2 - 55, width/2 - 50, height/2 - 45);
 
 
 }
