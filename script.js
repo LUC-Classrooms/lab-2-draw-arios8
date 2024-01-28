113362429a6148bfd9741a5fc6f43ee2666845a9
arios8 <154016447+arios8@users.noreply.github.com>
2024-01-28 17:06:34 +0000

    triangle

--- script.js
+++ script.js
@@ -5,8 +5,12 @@ function setup(){
 function draw(){
   background(176, 228, 247);
   // add your drawing code here ...
-  stroke(50,50)
-  ellipse(150,150,50)
+  stroke(50,50);
+  //width,height, circle size
+  //width/2 , height/2 = middle of canvas
+  ellipse(150,150,50);
+  //three points of shape
+  triangle(width/2 - 45, height/2 - 55, width/2 - 50, height/2 - 45);
 
 
 }
